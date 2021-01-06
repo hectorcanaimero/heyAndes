41ce6c156ba1b07a73d5beeaf53e8eb30b0e1fac
Héctor Velasquez <knaimero@gmail.com>
2021-01-06 15:41:12 -0300

    Page de detalle de la empresa

--- src/app/pages/empresas/detail/detail.component.ts
+++ src/app/pages/empresas/detail/detail.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
+
+import { DataService } from './../../../shared/services/data.service';
 
 @Component({
   selector: 'app-detail',
@@ -7,9 +12,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailComponent implements OnInit {
 
-  constructor() { }
+  title: string = '';
+  slug: Observable<string>;
+  items: Observable<any[]>;
+
+  constructor(
+    private router: Router,
+    private db: DataService,
+    private activated: ActivatedRoute,
+  ) { }
 
   ngOnInit(): void {
+    this.slug = this.activated.paramMap.pipe(map(paramsMap => paramsMap.get('slug')));
+    this.slug.subscribe((res) => this.getEmpresa(res));
   }
 
+  getEmpresa = (slug: string) => {
+    this.items = this.db.getEmpresaSlug(slug).pipe(tap((res) => this.title = res[0].nameAgency));
+  }
+
+  onBack = () => this.router.navigate(['empresas']);
 }
